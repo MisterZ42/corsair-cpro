2a2021384e9827f232ffd6084c4ced7106fb48b2
Marius Zachmann <mail@mariuszachmann.de>
2020-06-09 22:50:54 +0200

    GPL2 or later

--- corsair-cpro.c
+++ corsair-cpro.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * corsair-cpro.c - Linux driver for Corsair Commander Pro
  * Copyright (C) 2020 Marius Zachmann <mail@mariuszachmann.de>
